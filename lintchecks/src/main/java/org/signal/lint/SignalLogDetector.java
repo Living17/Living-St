SamSprung/Signal-Android

Add custom lint rule project.

lintchecks/src/main/java/org/signal/lint/SignalLogDetector.java
@@ -1,0 +1,77 @@
+package org.signal.lint;
+
+import com.android.tools.lint.client.api.JavaEvaluator;
+import com.android.tools.lint.detector.api.Category;
+import com.android.tools.lint.detector.api.Detector;
+import com.android.tools.lint.detector.api.Implementation;
+import com.android.tools.lint.detector.api.Issue;
+import com.android.tools.lint.detector.api.JavaContext;
+import com.android.tools.lint.detector.api.LintFix;
+import com.android.tools.lint.detector.api.Scope;
+import com.android.tools.lint.detector.api.Severity;
+import com.intellij.psi.PsiMethod;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.uast.UCallExpression;
+import org.jetbrains.uast.UExpression;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings("UnstableApiUsage")
+public final class SignalLogDetector extends Detector implements Detector.UastScanner {
+
+  static final Issue LOG_NOT_SIGNAL = Issue.create("LogNotSignal",
+                                                   "Logging call to Android Log instead of Signal's Logger",
+                                                   "Signal has its own logger which must be used.",
+                                                   Category.MESSAGES,
+                                                   5,
+                                                   Severity.ERROR,
+                                                   new Implementation(SignalLogDetector.class, Scope.JAVA_FILE_SCOPE));
+
+  @Override
+  public List<String> getApplicableMethodNames() {
+    return Arrays.asList("v", "d", "i", "w", "e", "wtf");
+  }
+
+  @Override
+  public void visitMethodCall(JavaContext context, @NotNull UCallExpression call, @NotNull PsiMethod method) {
+    JavaEvaluator evaluator = context.getEvaluator();
+
+    if (evaluator.isMemberInClass(method, "android.util.Log")) {
+      LintFix fix = quickFixIssueLog(call);
+      context.report(LOG_NOT_SIGNAL, call, context.getLocation(call), "Using 'android.util.Log' instead of a Signal Logger", fix);
+    }
+  }
+
+  private LintFix quickFixIssueLog(@NotNull UCallExpression logCall) {
+    List<UExpression> arguments  = logCall.getValueArguments();
+    String            methodName = logCall.getMethodName();
+    UExpression       tag        = arguments.get(0);
+
+    String fixSource = "org.thoughtcrime.securesms.logging.Log.";
+
+    switch (arguments.size()) {
+      case 2:
+        UExpression msgOrThrowable = arguments.get(1);
+        fixSource += String.format("%s(%s, %s)", methodName, tag, msgOrThrowable.asSourceString());
+        break;
+
+      case 3:
+        UExpression msg = arguments.get(1);
+        UExpression throwable = arguments.get(2);
+        fixSource += String.format("%s(%s, %s, %s)", methodName, tag, msg.asSourceString(), throwable.asSourceString());
+        break;
+
+      default:
+        throw new IllegalStateException("android.util.Log overloads should have 2 or 3 arguments");
+    }
+
+    String logCallSource = logCall.asSourceString();
+    LintFix.GroupBuilder fixGrouper = fix().group();
+    fixGrouper.add(fix().replace().text(logCallSource).shortenNames().reformat(true).with(fixSource).build());
+    return fixGrouper.build();
+  }
+
+
+}

lintchecks/src/test/java/org/signal/lint/LogDetectorTest.java
@@ -1,0 +1,57 @@
+package org.signal.lint;
+
+import org.junit.Test;
+
+import static com.android.tools.lint.checks.infrastructure.TestFiles.java;
+import static com.android.tools.lint.checks.infrastructure.TestLintTask.lint;
+
+public final class LogDetectorTest {
+
+  @Test
+  public void androidLogUsed_LogNotSignal_2_args() {
+    lint()
+      .files(
+        java("package foo;\n" +
+               "import android.util.Log;\n" +
+               "public class Example {\n" +
+               "  public void log() {\n" +
+               "    Log.d(\"TAG\", \"msg\");\n" +
+               "  }\n" +
+               "}")
+      )
+      .issues(SignalLogDetector.LOG_NOT_SIGNAL)
+      .run()
+      .expect("src/foo/Example.java:5: Error: Using 'android.util.Log' instead of a Signal Logger [LogNotSignal]\n" +
+                "    Log.d(\"TAG\", \"msg\");\n" +
+                "    ~~~~~~~~~~~~~~~~~~~\n" +
+                "1 errors, 0 warnings")
+      .expectFixDiffs("Fix for src/foo/Example.java line 5: Replace with org.thoughtcrime.securesms.logging.Log.d(\"TAG\", \"msg\"):\n" +
+                        "@@ -5 +5\n" +
+                        "-     Log.d(\"TAG\", \"msg\");\n" +
+                        "+     org.thoughtcrime.securesms.logging.Log.d(\"TAG\", \"msg\");");
+  }
+
+  @Test
+  public void androidLogUsed_LogNotSignal_3_args() {
+    lint()
+      .files(
+        java("package foo;\n" +
+               "import android.util.Log;\n" +
+               "public class Example {\n" +
+               "  public void log() {\n" +
+               "    Log.w(\"TAG\", \"msg\", new Exception());\n" +
+               "  }\n" +
+               "}")
+      )
+      .issues(SignalLogDetector.LOG_NOT_SIGNAL)
+      .run()
+      .expect("src/foo/Example.java:5: Error: Using 'android.util.Log' instead of a Signal Logger [LogNotSignal]\n" +
+                "    Log.w(\"TAG\", \"msg\", new Exception());\n" +
+                "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                "1 errors, 0 warnings")
+      .expectFixDiffs("Fix for src/foo/Example.java line 5: Replace with org.thoughtcrime.securesms.logging.Log.w(\"TAG\", \"msg\", new Exception()):\n" +
+                        "@@ -5 +5\n" +
+                        "-     Log.w(\"TAG\", \"msg\", new Exception());\n" +
+                        "+     org.thoughtcrime.securesms.logging.Log.w(\"TAG\", \"msg\", new Exception());");
+  }
+}

Living17/Living-St

lintchecks/src/main/java/org/signal/lint/SignalLogDetector.java
@@ -1,0 +1,77 @@
+package org.signal.lint;
+
+import com.android.tools.lint.client.api.JavaEvaluator;
+import com.android.tools.lint.detector.api.Category;
+import com.android.tools.lint.detector.api.Detector;
+import com.android.tools.lint.detector.api.Implementation;
+import com.android.tools.lint.detector.api.Issue;
+import com.android.tools.lint.detector.api.JavaContext;
+import com.android.tools.lint.detector.api.LintFix;
+import com.android.tools.lint.detector.api.Scope;
+import com.android.tools.lint.detector.api.Severity;
+import com.intellij.psi.PsiMethod;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.uast.UCallExpression;
+import org.jetbrains.uast.UExpression;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings("UnstableApiUsage")
+public final class SignalLogDetector extends Detector implements Detector.UastScanner {
+
+  static final Issue LOG_NOT_SIGNAL = Issue.create("LogNotSignal",
+                                                   "Logging call to Android Log instead of Signal's Logger",
+                                                   "Signal has its own logger which must be used.",
+                                                   Category.MESSAGES,
+                                                   5,
+                                                   Severity.ERROR,
+                                                   new Implementation(SignalLogDetector.class, Scope.JAVA_FILE_SCOPE));
+
+  @Override
+  public List<String> getApplicableMethodNames() {
+    return Arrays.asList("v", "d", "i", "w", "e", "wtf");
+  }
+
+  @Override
+  public void visitMethodCall(JavaContext context, @NotNull UCallExpression call, @NotNull PsiMethod method) {
+    JavaEvaluator evaluator = context.getEvaluator();
+
+    if (evaluator.isMemberInClass(method, "android.util.Log")) {
+      LintFix fix = quickFixIssueLog(call);
+      context.report(LOG_NOT_SIGNAL, call, context.getLocation(call), "Using 'android.util.Log' instead of a Signal Logger", fix);
+    }
+  }
+
+  private LintFix quickFixIssueLog(@NotNull UCallExpression logCall) {
+    List<UExpression> arguments  = logCall.getValueArguments();
+    String            methodName = logCall.getMethodName();
+    UExpression       tag        = arguments.get(0);
+
+    String fixSource = "org.whispersystems.libsignal.logging.Log.";
+
+    switch (arguments.size()) {
+      case 2:
+        UExpression msgOrThrowable = arguments.get(1);
+        fixSource += String.format("%s(%s, %s)", methodName, tag, msgOrThrowable.asSourceString());
+        break;
+
+      case 3:
+        UExpression msg = arguments.get(1);
+        UExpression throwable = arguments.get(2);
+        fixSource += String.format("%s(%s, %s, %s)", methodName, tag, msg.asSourceString(), throwable.asSourceString());
+        break;
+
+      default:
+        throw new IllegalStateException("android.util.Log overloads should have 2 or 3 arguments");
+    }
+
+    String logCallSource = logCall.asSourceString();
+    LintFix.GroupBuilder fixGrouper = fix().group();
+    fixGrouper.add(fix().replace().text(logCallSource).shortenNames().reformat(true).with(fixSource).build());
+    return fixGrouper.build();
+  }
+
+
+}

lintchecks/src/test/java/org/signal/lint/LogDetectorTest.java
@@ -1,0 +1,57 @@
+package org.signal.lint;
+
+import org.junit.Test;
+
+import static com.android.tools.lint.checks.infrastructure.TestFiles.java;
+import static com.android.tools.lint.checks.infrastructure.TestLintTask.lint;
+
+public final class LogDetectorTest {
+
+  @Test
+  public void androidLogUsed_LogNotSignal_2_args() {
+    lint()
+      .files(
+        java("package foo;\n" +
+               "import android.util.Log;\n" +
+               "public class Example {\n" +
+               "  public void log() {\n" +
+               "    Log.d(\"TAG\", \"msg\");\n" +
+               "  }\n" +
+               "}")
+      )
+      .issues(SignalLogDetector.LOG_NOT_SIGNAL)
+      .run()
+      .expect("src/foo/Example.java:5: Error: Using 'android.util.Log' instead of a Signal Logger [LogNotSignal]\n" +
+                "    Log.d(\"TAG\", \"msg\");\n" +
+                "    ~~~~~~~~~~~~~~~~~~~\n" +
+                "1 errors, 0 warnings")
+      .expectFixDiffs("Fix for src/foo/Example.java line 5: Replace with org.whispersystems.libsignal.logging.Log.d(\"TAG\", \"msg\"):\n" +
+                        "@@ -5 +5\n" +
+                        "-     Log.d(\"TAG\", \"msg\");\n" +
+                        "+     org.whispersystems.libsignal.logging.Log.d(\"TAG\", \"msg\");");
+  }
+
+  @Test
+  public void androidLogUsed_LogNotSignal_3_args() {
+    lint()
+      .files(
+        java("package foo;\n" +
+               "import android.util.Log;\n" +
+               "public class Example {\n" +
+               "  public void log() {\n" +
+               "    Log.w(\"TAG\", \"msg\", new Exception());\n" +
+               "  }\n" +
+               "}")
+      )
+      .issues(SignalLogDetector.LOG_NOT_SIGNAL)
+      .run()
+      .expect("src/foo/Example.java:5: Error: Using 'android.util.Log' instead of a Signal Logger [LogNotSignal]\n" +
+                "    Log.w(\"TAG\", \"msg\", new Exception());\n" +
+                "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                "1 errors, 0 warnings")
+      .expectFixDiffs("Fix for src/foo/Example.java line 5: Replace with org.whispersystems.libsignal.logging.Log.w(\"TAG\", \"msg\", new Exception()):\n" +
+                        "@@ -5 +5\n" +
+                        "-     Log.w(\"TAG\", \"msg\", new Exception());\n" +
+                        "+     org.whispersystems.libsignal.logging.Log.w(\"TAG\", \"msg\", new Exception());");
+  }
+}