84737fb51f20df05beacd7dfee3c86648252d03b
Alan Evans <alan@signal.org>
2020-05-19 19:32:16 -0300

    GroupsV2 All Squash - Internal Alpha 19

--- app/src/main/java/org/thoughtcrime/securesms/groups/GroupsV2CapabilityChecker.java
+++ app/src/main/java/org/thoughtcrime/securesms/groups/GroupsV2CapabilityChecker.java
@@ -3,6 +3,7 @@ package org.thoughtcrime.securesms.groups;
 import androidx.annotation.NonNull;
 import androidx.annotation.WorkerThread;
 
+import org.GV2DebugFlags;
 import org.thoughtcrime.securesms.dependencies.ApplicationDependencies;
 import org.thoughtcrime.securesms.jobs.RetrieveProfileJob;
 import org.thoughtcrime.securesms.logging.Log;
@@ -36,11 +37,27 @@ final class GroupsV2CapabilityChecker {
   boolean allSupportGroupsV2AndUuid(@NonNull Collection<RecipientId> recipientIds)
       throws IOException
   {
+    if (GV2DebugFlags.SKIP_CAPABILITY_CHECK) return true;
+
     final HashSet<RecipientId> recipientIdsSet = new HashSet<>(recipientIds);
+
+    if (GV2DebugFlags.EXTRA_LOGGING_AND199) {
+      for (RecipientId recipientId : recipientIdsSet) {
+        Recipient member = Recipient.resolved(recipientId);
+        Log.d("ALAN", String.format("Capability: %s %s", member.toShortString(ApplicationDependencies.getApplication()), member.getGroupsV2Capability()));
+      }
+    }
 
     for (RecipientId recipientId : recipientIdsSet) {
       Recipient            member         = Recipient.resolved(recipientId);
       Recipient.Capability gv2Capability  = member.getGroupsV2Capability();
+
+      if (GV2DebugFlags.FORCE_SELF_CAPABILITIES) {
+        if (member.isLocalNumber() && gv2Capability != Recipient.Capability.SUPPORTED) {
+          Log.d("ALAN", String.format("Forcing own gv2 capability from %s to %s", gv2Capability, Recipient.Capability.SUPPORTED));
+          gv2Capability = Recipient.Capability.SUPPORTED;
+        }
+      }
 
       if (gv2Capability == Recipient.Capability.UNKNOWN) {
         if (!ApplicationDependencies.getJobManager().runSynchronously(RetrieveProfileJob.forRecipient(member), TimeUnit.SECONDS.toMillis(1000)).isPresent()) {
